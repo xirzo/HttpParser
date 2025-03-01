ac20af7abb9201a7d80774c9679b4f147cf3a3df
xirzo <easttem@gmail.com>
2025-03-01 18:43:27 +0300

    test: parse status line

---
+++ tests/tests.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "http_parser.h"
+
+char raw_request[] =
+    "GET /index.html HTTP/1.1\r\n"
+    "Host: localhost:5000\r\n"
+    "User-Agent: curl/7.68.0\r\n"
+    "\r\n";
+
+void test_parse_request_line() {
+    char raw_request_line[] = "GET /index.html HTTP/1.1\r\n";
+    HttpRequest *r = malloc(sizeof(*r));
+
+    parse_request_line(r, raw_request_line);
+
+    assert(r->method == GET);
+    assert(strcmp(r->uri, "/index.html") == 0);
+    assert(r->version == HTTP_1_1);
+
+    free(r);
+}
+
+int main() {
+    test_parse_request_line();
+    printf("All tests passed!\n");
+    return EXIT_SUCCESS;
+}
